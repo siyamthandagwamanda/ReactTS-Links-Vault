3ce6fd68c74aaa6a752cac50b93aacec9d9bd61d
siyamthandagwamanda <gwamandasiyamthanda13@gmail.com>
2026-07-23 09:13:00 +0200

    -------

--- src/Types/vault.ts
+++
@@ -1,8 +0,0 @@
-export interface LinkItem {
-    id: string,
-    title: string,
-    url: string,
-    description: string,
-    tag?: string,
-}
-export type ModelState = 'NONE' | 'ADD_SEARCH' | 'MANAGE_DIRECTORY'
