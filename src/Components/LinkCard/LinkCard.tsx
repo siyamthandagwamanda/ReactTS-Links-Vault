4e9b76e62be7445eeb22de242c7e2cfaf84d0c7b
siyamthandagwamanda <gwamandasiyamthanda13@gmail.com>
2026-07-23 15:17:19 +0200

    individual web link

---
+++ src/Components/LinkCard/LinkCard.tsx
@@ -0,0 +1,10 @@
+import type { LinkItem } from "../../Types/Vault";
+import './LinkCard.module.css';
+
+interface LinkCardProps{
+    link: LinkItem;
+}
+
+export default function LinkCard(){
+    return
+}
