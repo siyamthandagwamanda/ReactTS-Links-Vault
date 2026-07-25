f0675e20543104a17ada76ce88f724fcab5336e2
Thulile18 <busisiwegqada0@gmail.com>
2026-07-25 11:53:21 +0200

    receives 'children' and 'close'

--- src/Components/Modal/Modal.tsx
+++ src/Components/Modal/Modal.tsx
@@ -4,3 +4,17 @@ interface ModalProps{
     chidren: React.ReactNode;
     close: () => void;
 }
+
+function Modal({ chidren, close } : ModalProps){
+    return(
+        <div className="overlay">
+            <div className="modal">
+                <button className="close-button" onClick={close}>
+                     ×
+                </button>
+                {chidren}
+            </div>
+        </div>
+    );
+}
+export default Modal;
