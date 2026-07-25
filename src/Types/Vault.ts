3dd6d1b3b7307cd588e1d44a8c350cac008740c0
Thulile18 <busisiwegqada0@gmail.com>
2026-07-25 10:01:22 +0200

    ------

--- src/Types/Vault.ts
+++
@@ -1,9 +0,0 @@
-export interface LinkItem{
-    id: string,
-    title: string,
-    url: string,
-    description: string,
-    tag?: string,
-}
-
-export type ModalState = 'NONE' | 'ADD_SEARCH' | 'MANAGE_DIRECTORY';
