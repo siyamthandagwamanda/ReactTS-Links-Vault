a689342f69eace61d694c7e85f77dd08ed9680db
gwamandasiyamthanda13@gmail.com <gwamandasiyamthanda13@gmail.com>
2026-08-13 00:13:08 +0200

    define props interface for link

--- src/Components/LinkForm/LinkForm.tsx
+++ src/Components/LinkForm/LinkForm.tsx
@@ -1,3 +1,10 @@
 import { useState } from "react";
 import Modal from "../Modal/Modal";
 import type { Link } from "../../Types/Link";
+
+interface LinkFormProps{
+  editingLink: Link | null;
+  onAdd: (link: Omit<Link, "id">) => void;
+  onUpdate: (id: number, updates: Omit<Link, "id">) => void;
+  close: () => void;
+}
